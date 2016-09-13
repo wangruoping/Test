8840ca1e52c7f2c9b3d1d21d21fa34327c64e4d5
liuxinxuexi <hjhx@DESKTOP-5LMF1HR>
2016-09-13 21:25:31 +0800

    1

--- src/cn/pxl/app/ms/controller/LoginController.java
+++ src/cn/pxl/app/ms/controller/LoginController.java
@@ -1,11 +1,16 @@
 package cn.pxl.app.ms.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import cn.pxl.app.ms.entity.CompanyUserEntity;
 import cn.pxl.app.ms.service.UserService;
@@ -13,7 +18,7 @@ import cn.pxl.app.ms.service.UserService;
 @Controller
 public class LoginController {
 
-	@Resource(name="userService")
+	@Resource(name = "userService")
 	private UserService userService;
 
 	@RequestMapping("/index.html")
@@ -21,17 +26,21 @@ public class LoginController {
 		return "index";
 	}
 
-	@RequestMapping(value="/login.html", method=RequestMethod.POST)
-	public String login(@RequestParam(value="username", required=true) String username,
-			@RequestParam(value="password", required=true) String password) {
+	@RequestMapping(value = "/login.html", method = RequestMethod.POST)
+	public ModelAndView login(@RequestParam(value = "username", required = true) String username,
+			@RequestParam(value = "password", required = true) String password, HttpSession session) {
+		
+		Map<String, Object> map = new HashMap<String, Object>();
 		
 		CompanyUserEntity companyUserEntity = userService.getUserInfoByNameAndPass(username, password);
 		if (companyUserEntity != null) {
-			//登陆成功
-			return "main";
+			session.setAttribute("userInfo", companyUserEntity);
+			// 登陆成功
+			map.put("userInfo", companyUserEntity);
+			return new ModelAndView("main", map);
 		} else {
-			//登陆失败
-			return "index";
+			// 登陆失败
+			return new ModelAndView("index", map);
 		}
 	}
 }
