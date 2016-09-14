8c5105294d73c0edbd9d04c3803a4d6eb303b9ca
liuxinxuexi <hjhx@DESKTOP-5LMF1HR>
2016-09-14 11:02:05 +0800

    1

---
+++ src/cn/pxl/app/ms/entity/CompanyTemplateEntity.java
@@ -0,0 +1,61 @@
+package cn.pxl.app.ms.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author liuxin
+ * 
+ *         模板信息表
+ * 
+ */
+@Entity
+@Table(name = "company_template")
+public class CompanyTemplateEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String id;
+	// 模板名称
+	private String name;
+	// 模板宽度
+	private Integer width;
+	// 模板高度
+	private Integer height;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getWidth() {
+		return width;
+	}
+
+	public void setWidth(Integer width) {
+		this.width = width;
+	}
+
+	public Integer getHeight() {
+		return height;
+	}
+
+	public void setHeight(Integer height) {
+		this.height = height;
+	}
+
+}
