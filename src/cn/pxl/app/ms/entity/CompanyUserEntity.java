15f274a490b5789c3c1278b0af1f0952898e6a24
liuxinxuexi <hjhx@DESKTOP-5LMF1HR>
2016-09-18 17:10:30 +0800

    1

--- src/cn/pxl/app/ms/entity/CompanyUserEntity.java
+++ src/cn/pxl/app/ms/entity/CompanyUserEntity.java
@@ -6,9 +6,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.GeneratorType;
-import org.hibernate.annotations.GenericGenerator;
-
 /**
  * @author liuxin
  * 
